716e478db3db952d777b432ce5e89dfc18fde136
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-21 23:52:39 -0300

    Estilos del Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
-      <h2>Desde Layout</h2>
-      <Outlet />
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/5 bg-sky-600 px-5 py-10">
+        <h2 className="text-3xl text-slate-900 font-bold text-center">
+          CRM-Clientes
+        </h2>
+        <nav className="mt-10">
+          <Link
+            className="text-2xl text-slate-900 block mt-2 hover:text-slate-600"
+            to="/clientes"
+          >
+            Clientes
+          </Link>
+          <Link
+            className="text-2xl text-slate-900 block mt-2 hover:text-slate-600"
+            to="/clientes/nuevo"
+          >
+            Nuevo Cliente
+          </Link>
+        </nav>
+      </div>
+      <div className="md:w-3/5">
+        <Outlet />
+      </div>
     </div>
   );
 };
